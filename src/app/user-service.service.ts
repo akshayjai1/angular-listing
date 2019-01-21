3d340300a79db21f661e78baef859f268f5c667a
sangeeta2288 <ashrit.sangeeta@gmail.com>
2019-01-21 15:11:27 -0500

    add user-service

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -31,17 +31,5 @@ export class UserServiceService {
         );
     });
     return promise;
-    // return this.http.get('http://dummy.restapiexample.com/api/v1/employees').toPromise()
-    //             .then( res => {
-    //               this.users = res.map(item => {
-    //           return new SearchItem(
-    //             item.trackName,
-    //             item.artistName,
-    //             item.trackViewUrl,
-    //             item.artworkUrl30,
-    //             item.artistId
-    //           );
-    //         });
-    //             .subscribe(response => this.users = response);
   }
 }
